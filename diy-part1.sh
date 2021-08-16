2a1d6a3b8ec0f5b0d61d70d5f954e1399f909468
yulu2000 <yulu_2000@sina.com>
2021-08-16 15:25:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+echo 'src-git sirpdboy-package https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
